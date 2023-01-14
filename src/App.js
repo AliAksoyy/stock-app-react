bd38bda56ce1ea463705a5167ac9a442c48f7b26
AliAksoyy <alifeyza4017@gmail.com>
2023-01-14 22:38:26 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import  Box  from '@mui/material/Box';
+import  Typography  from '@mui/material/Typography';
+import Ali from './Ali';
+
+
+function App() {
+  return (
+    <div>
+      <Box>
+        <Typography>Ali</Typography>
+        <Ali />
+      </Box>
+    </div>
+  );
+}
+
+export default App;
