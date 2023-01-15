9f6ce338478adaa0e83f85ad306b36e395d5f9f6
AliAksoyy <alifeyza4017@gmail.com>
2023-01-16 00:23:43 +0300

    v11

--- src/hooks/useAuthCalls.jsx
+++ src/hooks/useAuthCalls.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { fetchFail, fetchStart, registerSuccess,loginSuccess } from '../features/authSlice'
+import { fetchFail, fetchStart, registerSuccess,loginSuccess, logoutSuccess } from '../features/authSlice'
 import { toastifyError, toastifySuccess } from '../helpers/toastify'
 
 
@@ -32,17 +32,32 @@ const login=async(value)=> {
     const {data}=  await axios.post(`${BASE_URL}account/auth/login/`, value)
         navigate("/stock")
         dispatch(loginSuccess(data))
-        toastifySuccess("Login can not be performed")
+        toastifySuccess("Login performed")
 
     }catch(error){
-        toastifyError(`${error}`)
+        toastifyError(`Login can not be performed`)
+        dispatch(fetchFail())
+        console.log(error)
+
+    }
+}
+const logout=async()=> {
+    dispatch(fetchStart())
+    try{
+        await axios.post(`${BASE_URL}account/auth/logout/`)
+        navigate("/login")
+        dispatch(logoutSuccess())
+        toastifySuccess("Logout performed")
+
+    }catch(error){
+        toastifyError(`Logout can not be performed`)
         dispatch(fetchFail())
         console.log(error)
 
     }
 }
 
-  return {register,login}
+  return {register,login,logout}
 }
 
 export default useAuthCalls
