1fb3658af3329eaaf00c8b66ab31657effc36908
AliAksoyy <alifeyza4017@gmail.com>
2023-01-16 20:07:18 +0300

    v16

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter"
 import '@tremor/react/dist/esm/tremor.css';
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { persistor, store } from "./app/store";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { grey, blueGrey } from "@mui/material/colors";
-
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
 
@@ -32,10 +32,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
      <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <AppRouter />
         <ToastContainer />
-       </BrowserRouter>   
+       </BrowserRouter> 
+       </PersistGate>  
       </Provider>
       </ThemeProvider>
   );
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,26 @@
 import {configureStore} from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
 import stockReducer from "../features/stockSlice"
-export const store=configureStore({
-    reducer:{
-        auth: authReducer,
-        stock:stockReducer
-    },
-})
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+  const persistedReducer = persistReducer(persistConfig, authReducer)
+   
+ 
+
+  export const store=configureStore({
+      reducer:{
+          auth: persistedReducer,
+          stock:stockReducer
+      },
+  })
+    export const persistor = persistStore(store)
+    
+  
